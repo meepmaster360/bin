040a3fa0c4e7ed2bf5a0b1a9ed2af06f97461126
vsko <vsko@email.ua>
2022-05-03 17:20:46 +0300

    Fixed sshfs-server.sh

--- sshfs-server.sh
+++ sshfs-server.sh
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
-=======
-#!/bin/bash
->>>>>>> 1617387b6792de6fc3e1a5fc5dd9a5454684df47
 
 # macOS
 sshfs -o IdentityFile=~/.ssh/id_rsa vsko@192.168.1.200:/ ~/server
 
 # Debian / Ubuntu
-<<<<<<< HEAD
 #sudo sshfs -o allow_other, IdentityFile = ~/.ssh/id_rsa vsko@192.168.1.200:/home/user/ /mnt/share
 
-=======
-# sudo sshfs -o allow_other, IdentityFile = ~/.ssh/id_rsa vsko@192.168.1.200:/home/user/ /mnt/share
->>>>>>> 1617387b6792de6fc3e1a5fc5dd9a5454684df47
